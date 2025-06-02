acb3eecb0de5e230f666fafa6cfe6d4cecfb3f97
netisdark <pergap3@gmail.com>
2025-06-02 18:01:48 +0545

    added package.json and added build command

--- server/server.js
+++ server/server.js
@@ -12,6 +12,12 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.json());
 
+app.use(express.static(path.join(__dirname, '../client/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
 const subscribersFile = path.join(__dirname, 'subscribers.json');
 
 app.post('/api/notify', async (req, res) => {
@@ -22,7 +28,6 @@ app.post('/api/notify', async (req, res) => {
   }
 
   try {
-    // 1. Read existing subscribers
     let subscribers = [];
     if (fs.existsSync(subscribersFile)) {
       const data = fs.readFileSync(subscribersFile, 'utf8');
@@ -65,5 +70,5 @@ app.post('/api/notify', async (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+  console.log(`Server running at port: ${PORT}`);
 });
